dc4e1b26926377fae708022516aa810f316deb0a
Sedat Polat <sedatpolat355@gmail.com>
2024-10-27 15:22:00 +0300

    delete-popup completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,35 @@
 import { FaRegTrashAlt } from "react-icons/fa"
-import {Delete_popup} from "./components/Delete_popup"
+import { Delete_popup } from "./components/Delete_popup"
+import { useState } from "react"
 
 function App() {
-  return (
-    <div className="app flex items-center justify-center min-h-screen">
-      <div className="delete-btn flex items-center justify-center gap-x-2 bg-red-500 px-8 py-2 text-white rounded-md text-xl cursor-pointer">
-        <FaRegTrashAlt />
-        <button>Delete</button>
-      </div>
+  const [open, setOpen] = useState(false)
 
-      <Delete_popup/>
+  console.log(open)
+
+  return (
+    <div className="app">
+      <button className="btn btn-danger" onClick={() => setOpen(true)}>
+        <FaRegTrashAlt /> Delete
+      </button>
+
+      <Delete_popup open={open} onClose={() => setOpen(false)}>
+        <div className="text-center w-56">
+          <FaRegTrashAlt size={56} className="mx-auto text-red-500" />
+
+          <div className="mx-auto my-4 w-48">
+            <h3 className="text-lg font-black text-gray-800">Confirm Delete</h3>
+            <p className="text-sm text-gray-500">
+              Are your sure you want to delete this item?
+            </p>
+          </div>
+
+          <div className="flex gap-4">
+            <button className="btn btn-danger w-full">Delete</button>
+            <button onClick={() => setOpen(false)} className="btn btn-light w-full">Cancel</button>
+          </div>
+        </div>
+      </Delete_popup>
     </div>
   )
 }
--- src/components/Delete_popup.jsx
+++ src/components/Delete_popup.jsx
@@ -1,20 +1,28 @@
 import { FaRegTrashAlt } from "react-icons/fa"
 
-const Delete_popup = () => {
-  return(
-    <div className="popup-container">
-      <div className="overlay"></div>
-      
-      <div className="popup">
-        <FaRegTrashAlt/>
-        <div className="desc">
-          <h2>Confirm Delete</h2>
-          <p>Are you sure you want to delete this item?</p>
-        </div>
-        <div className="buttons">
-          <button>Delete</button>
-          <button>Cancel</button>
-        </div>
+const Delete_popup = ({ open, onClose, children }) => {
+  return (
+    // overlay
+    <div
+      onClick={onClose}
+      className={`
+        fixed inset-0 flex items-center justify-center transition-colors 
+        ${open ? "visible bg-black/20" : "invisible"}
+      `}
+    >
+      {/* popup */}
+      <div
+        onClick={(e) => e.stopPropagation()} 
+        className={`
+        bg-white rounded-xl shadow transition-all p-6
+        ${open ? "scale-100 opacity-100" : "scale-125 opacity-0"}
+      `}>
+        <button 
+          onClick={onClose}
+          className="absolute top-2 right-2 px-2 py-1 rounded-full text-gray-400 bg-slate-300 hover:bg-slate-700 hover:text-gray-100">
+          &#10006;
+        </button>
+        {children}
       </div>
     </div>
   )
